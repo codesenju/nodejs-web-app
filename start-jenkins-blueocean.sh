25854fe2ea432d8324aa8acf36b8e2586742a9f5
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-06-18 15:13:48 +0200

    Delete start-jenkins-blueocean.sh

--- start-jenkins-blueocean.sh
+++
@@ -1,6 +0,0 @@
-docker container run --name jenkins --rm --detach \
-  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
-  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
-  --volume "$PWD"/jenkins-data:/var/jenkins_home \
-  --volume "$PWD"/jenkins-docker-certs:/certs/client:ro \
-  --publish 8080:8080 --publish 50000:50000 jenkinsci/blueocean
