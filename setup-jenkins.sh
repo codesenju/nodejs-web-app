3867d4b230ec9b74f000af37a810011f28134cc0
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-06-18 15:13:34 +0200

    Update setup-jenkins.sh

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+
 mkdir -pv $PWD/jenkins-docker-certs
 mkdir -pv $PWD/jenkins-data
 
+docker rm -f jenkins-docker
 docker container run --name jenkins-docker --rm --detach \
   --privileged --network jenkins --network-alias docker \
   --env DOCKER_TLS_CERTDIR=/certs \
   --volume "$PWD"/jenkins-docker-certs:/certs/client \
   --volume "$PWD/"jenkins-data:/var/jenkins_home \
   --publish 2376:2376 docker:dind
-
+  
+docker rm -f jenkins
 docker container run --name jenkins --rm --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
