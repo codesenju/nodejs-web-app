4352bbbce3faca763154c6afc96d76476fc02595
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-06-18 15:11:31 +0200

    Update start-jenkins-docker.sh

--- start-jenkins-docker.sh
+++ start-jenkins-docker.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+mkdir -pv $PWD/jenkins-docker-certs
+mkdir -pv $PWD/jenkins-data
+
 docker container run --name jenkins-docker --rm --detach \
   --privileged --network jenkins --network-alias docker \
   --env DOCKER_TLS_CERTDIR=/certs \
   --volume "$PWD"/jenkins-docker-certs:/certs/client \
   --volume "$PWD/"jenkins-data:/var/jenkins_home \
   --publish 2376:2376 docker:dind
+
+docker container run --name jenkins --rm --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --volume "$PWD"/jenkins-data:/var/jenkins_home \
+  --volume "$PWD"/jenkins-docker-certs:/certs/client:ro \
+  --publish 8080:8080 --publish 50000:50000 jenkinsci/blueocean
