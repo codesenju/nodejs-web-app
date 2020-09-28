7243bbd2ce0918b0cb9614b0a9d93bb54806e5f2
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-09-28 11:52:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello ifinance-gli-dev');
+  res.send('Hello ifinance');
 });
 
 app.listen(PORT, HOST);
